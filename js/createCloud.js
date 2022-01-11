e0eb7d9deda64b199091f1044c4ef67e962f836d
kryban93 <kryban1993@gmail.com>
2022-01-11 09:08:35 +0100

    added created cloud function and create sky function

---
+++ js/createCloud.js
@@ -0,0 +1,29 @@
+import * as THREE from 'https://threejs.org/build/three.module.js';
+
+export function createCloud(numberOfBlocks) {
+	const cloud = new THREE.Object3D();
+	const geometry = new THREE.BoxGeometry(20, 20, 20);
+	const material = new THREE.MeshPhongMaterial({
+		color: 0xffffff,
+	});
+
+	const blocksInCloud = numberOfBlocks < 3 ? 3 : numberOfBlocks;
+
+	for (let i = 0; i < blocksInCloud; i++) {
+		let box = new THREE.Mesh(geometry, material);
+		box.position.x = i * 15;
+		box.position.y = Math.random() * 10;
+		box.position.z = Math.random() * 10;
+		box.rotation.z = Math.random() * Math.PI * 2;
+		box.rotation.y = Math.random() * Math.PI * 2;
+
+		const scale = 0.2 + Math.random() * 0.9;
+		box.scale.set(scale, scale, scale);
+		box.castShadow = true;
+		box.receiveShadow = true;
+
+		cloud.add(box);
+	}
+
+	return cloud;
+}
